7c0d235005a145d07a34f77b5cb19afc1ecd1ec6
Donna <ntang800@gmail.com>
2023-07-21 15:19:02 -0600

    first commit

---
+++ lesson1/heart_rate.py
@@ -0,0 +1,6 @@
+age = int(input('Enter your age: '))
+max_rate = 220 - age
+slowest = max_rate * 0.65
+fastest = max_rate * 0.85
+print(f"when exercising to strngthen your heart, you should keep your heart rate betwwen {slowest:.0f} and {fastest:.0f} of your heart's naxinmunm rate.")
+
